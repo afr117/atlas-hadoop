152d2efb492ea95eb93d71ba05c26f716184f42a
Alfredo Figueroa <1947chuopita@gmail.com>
2026-03-23 01:21:16 +0000

    load files

--- hadoop_project/createdirectories.sh
+++ hadoop_project/createdirectories.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+<<<<<<< HEAD
 # Create /holbies directory
 hadoop fs -mkdir -p /holbies
 
 # Create /holbies/input directory
 hadoop fs -mkdir -p /holbies/input
+=======
+/root/atlas-hadoop/hadoop-3.3.2/bin/hadoop fs -mkdir -p /holbies
+/root/atlas-hadoop/hadoop-3.3.2/bin/hadoop fs -mkdir -p /holbies/input
+>>>>>>> dfb1b5d (load files)
